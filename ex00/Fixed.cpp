b5a45ce87e1fcac5de057dcc6a30d8e2bdac7bf0
nguinot- <nguinot-@f4r12s2.paris.42.school>
2026-04-22 14:49:36 +0200

    premier push

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,36 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() : _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &src) : _value(src._value)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+		_value = rhs._value;
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_value = raw;
+}
---
+++ ex00/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+	private:
+		int	_value;
+		static const int _fractionBits = 8;
+
+	public:
+		Fixed();
+		Fixed(const Fixed &src);
+		Fixed &operator=(const Fixed &rhs);
+		~Fixed();
+
+	int	getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+};
+
+
+#endif
